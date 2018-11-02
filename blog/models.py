e7f5863baee4c933ad71b52ffae308a4acb6ccad
warne2tj <warne2tj@cmich.edu>
2018-11-02 12:35:32 -0400

    Changed the formatting on the template to display blog posts. Pushing to see if it works on the live site.

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
     title = models.CharField(max_length = 200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank = True, null = True)
 
     def publish(self):
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    context = {
+        "posts": posts,
+    }
+    return render(request, 'blog/post_list.html', context)
