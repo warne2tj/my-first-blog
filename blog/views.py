5b61dcdeb2da014d21b2325d44b37aca22cf01bc
warne2tj <warne2tj@cmich.edu>
2018-11-02 15:46:13 -0400

    It has been a while since the last commit. This commit added more options for the blog posts. It updated the html files to extended html files. When you click a blog post it takes you to a seperate url to display only that blog post.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 # Create your views here.
@@ -12,3 +12,10 @@ def post_list(request):
         "posts": posts,
     }
     return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    print ("hello")
+    print (pk)
+    post = get_object_or_404(Post, pk=pk)
+    print (post)
+    return render(request, 'blog/post_detail.html', {'post': post})
